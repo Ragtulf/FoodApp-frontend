b7ce36f28a8cff0d4e3e84d756d965a21440b772
Ragna <ragnaulfars@gmail.com>
2020-06-02 14:54:20 +0200

    Made the card clickable. Made a fetch to get a specific recipe

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 export const Feed = () => {
   const [recipe, setRecipe] = useState([])
@@ -16,13 +17,15 @@ export const Feed = () => {
     <div>
       {recipe.map((item) => (
         <div>
-          <p>{item.title}</p>
-          <p>Ragnhildur Elín</p>
-          <img src={item.image} alt={item.title} />
-          <p>{item.shortDescription}</p>
-          {item.tags.map((tag) => (
-            <p>{tag}</p>
-          ))}
+          <Link to={`/recipe/${item._id}`}>
+            <p>{item.title}</p>
+            <p>Ragnhildur Elín</p>
+            <img src={item.image} alt={item.title} />
+            <p>{item.shortDescription}</p>
+            {item.tags.map((tag) => (
+              <p>{tag}</p>
+            ))}
+          </Link>
         </div>
       ))}
     </div>
--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 export const Recipe = () => {
+  const [recipe, setRecipe] = useState([])
+  const { id } = useParams()
+
+  useEffect(() => {
+    fetch(`https://grymt-food-app.herokuapp.com/recipe/${id}`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log('Details:', json)
+        setRecipe(json)
+      })
+  }, [id])
+
   return (
     <div>
-      <p>Hejdå!</p>
+      <p>{recipe.title}</p>
     </div>
   )
 }
