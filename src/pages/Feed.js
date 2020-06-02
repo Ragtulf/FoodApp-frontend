d6478c6512f900e11641d854bbd12a5828f431c5
Ragna <ragnaulfars@gmail.com>
2020-06-02 14:12:40 +0200

    Rendered info on card

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,19 +1,30 @@
 import React, { useState, useEffect } from 'react'
 
 export const Feed = () => {
-  // const [recipe, setRecipe] = useState([])
+  const [recipe, setRecipe] = useState([])
 
   useEffect(() => {
     fetch('https://grymt-food-app.herokuapp.com/recipes')
       .then((res) => res.json())
       .then((json) => {
         console.log('This is json:', json)
+        setRecipe(json)
       })
-  })
+  }, [])
 
   return (
     <div>
-      <p>Hej!</p>
+      {recipe.map((item) => (
+        <div>
+          <p>{item.title}</p>
+          <p>Ragnhildur Elín</p>
+          <img src={item.image} alt={item.title} />
+          <p>{item.shortDescription}</p>
+          {item.tags.map((tag) => (
+            <p>{tag}</p>
+          ))}
+        </div>
+      ))}
     </div>
   )
 }
